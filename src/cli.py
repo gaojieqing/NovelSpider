1f6bc0f51b2ca6f426701510cad2fb9053276c59
Eason Gao <jieqing.gao@ximalaya.com>
2025-09-28 16:03:28 +0800

    增加新spider

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,11 @@
-from spiders.spider import ShuhaigeSpider, QQSpider, QisiSpider
+from spiders.spider import ShuhaigeSpider, QQSpider, QisiSpider, BaodeSpider, Bxwxw7Spider
 
 if __name__ == '__main__':
     # spider = QQSpider()
     # spider.download(46557853)
-    spider = ShuhaigeSpider()
-    spider.download('236281')
+    # spider = ShuhaigeSpider()
+    # spider.download('236281')
+    # spider = BaodeSpider()
+    # spider.download('15185')
+    spider = Bxwxw7Spider()
+    spider.download('205281')
--- src/spiders/spider.py
+++ src/spiders/spider.py
@@ -145,6 +145,153 @@ class ShuhaigeSpider(Spider):
             response = self.s.get(url)
         return response
 
+
+# 欧巴小说网
+class BaodeSpider(Spider):
+    s = requests.Session()
+
+    def download(self, book_id):
+        super().download(book_id)
+        book_title, chapters = self.fetch_book(book_id)
+        print(f'{book_title} 开始下载')
+        chapter_ids = list(chapters.values())
+        chapter_ids.sort()
+        folder_path = os.path.dirname(os.path.abspath(__file__))
+        while os.path.basename(folder_path) != "NovelSpider":
+            folder_path = os.path.dirname(folder_path)
+        file_path = os.path.join(folder_path, f'docs/{book_title}.md')
+        with open(file_path, 'w') as f:
+            for chapter_id in chapter_ids:
+                chapter_title, contents = self.fetch_chapter(book_id, chapter_id)
+                f.write(f'# {chapter_title}\n')
+                f.write('\n'.join(contents))
+                f.write('\n\n')
+                print(f'{chapter_title} 下载完成')
+
+    def fetch_book(self, book_id: int):
+        url = f'https://www.baode.cc/ob/{book_id}/'
+        response = self.get_retryable(url)
+
+        book_title = ''
+        chapters = {}
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            book_title = soup.find('meta', {'property': 'og:novel:book_name'})['content']
+            for dd in soup.find('div', {'id': 'list'}).find_all('dd'):
+                a = dd.find('a')
+                chapters[a.text] = int(re.findall(r'\d+', a['href'])[1])
+        return book_title, chapters
+
+    def fetch_chapter(self, book_id: int, chapter_id: int):
+        chapter_title = ''
+        contents = []
+        page = 1
+        has_next = True
+        while has_next:
+            has_next = False
+            url = f'https://www.baode.cc/ob/{book_id}/{chapter_id}.html'
+            if page > 1:
+                url = f'https://www.baode.cc/ob/{book_id}/{chapter_id}_{page}.html'
+            print(url)
+            response = self.get_retryable(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                chapter_title = soup.find('div', {'class': 'bookname'}).find('h1').text
+                content = soup.find('div', {'id': 'content'}).get_text('\n')
+                content = content.replace('		', '')
+                contents.append(content)
+                # for p in soup.find('div', {'id': 'content'}).find_all('p'):
+                #     content = p.text
+                #     if '请点击下一页继续阅读' in content:
+                #         has_next = True
+                #     elif '请大家收藏：' not in content:
+                #         contents.append(content)
+            else:
+                print(f'{url} {response.status_code}')
+            page += 1
+        return chapter_title, contents
+
+    def get_retryable(self, url: str):
+        response = self.s.get(url)
+
+        if response.status_code == 403:
+            response = self.s.get(url)
+        return response
+
+
+# 笔下文学
+class Bxwxw7Spider(Spider):
+    s = requests.Session()
+
+    def download(self, book_id):
+        super().download(book_id)
+        book_title, chapters = self.fetch_book(book_id)
+        print(f'{book_title} 开始下载')
+        chapter_ids = list(chapters.values())
+        chapter_ids.sort()
+        folder_path = os.path.dirname(os.path.abspath(__file__))
+        while os.path.basename(folder_path) != "NovelSpider":
+            folder_path = os.path.dirname(folder_path)
+        file_path = os.path.join(folder_path, f'docs/{book_title}.md')
+        with open(file_path, 'w') as f:
+            for chapter_id in chapter_ids:
+                chapter_title, contents = self.fetch_chapter(book_id, chapter_id)
+                f.write(f'# {chapter_title}\n')
+                f.write('\n'.join(contents))
+                f.write('\n\n')
+                print(f'{chapter_title} 下载完成')
+
+    def fetch_book(self, book_id: str):
+        url = f'https://www.bxwxw7.com/article/{book_id}/'
+        response = self.get_retryable(url)
+
+        book_title = ''
+        chapters = {}
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            book_title = soup.find('meta', {'property': 'og:novel:book_name'})['content']
+            for dd in soup.find('div', {'id': 'list'}).find_all('dd'):
+                a = dd.find('a')
+                if a.has_attr('href'):
+                    chapters[a.text] = int(re.findall(r'\d+', a['href'])[1])
+        else:
+            print(f'{url} {response.status_code}')
+        return book_title, chapters
+
+    def fetch_chapter(self, book_id: int, chapter_id: int):
+        chapter_title = ''
+        contents = []
+        page = 1
+        has_next = True
+        while has_next:
+            has_next = False
+            url = f'https://www.bxwxw7.com/{book_id}/{chapter_id}.html'
+            if page > 1:
+                url = f'https://www.bxwxw7.com/{book_id}/{chapter_id}_{page}.html'
+            print(url)
+            response = self.get_retryable(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                chapter_title = soup.find('div', {'class': 'con_top'}).find('h1').text
+                content = soup.find('div', {'id': 'booktxt'}).get_text('\n')
+                content = content.replace('　　', '\n')
+                contents.append(content)
+            else:
+                print(f'{url} {response.status_code}')
+            page += 1
+        return chapter_title, contents
+
+    def get_retryable(self, url: str):
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
+        }
+        response = self.s.get(url=url, headers=headers)
+
+        if response.status_code == 403:
+            response = self.s.get(url=url, headers=headers)
+        return response
+
+
 # 七四小说
 class QisiSpider(Spider):
     s = requests.Session()
